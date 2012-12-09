f260873c9f506065fa44b02db25d81636734fd89
William <logmystuff@gmail.com>
2012-12-09 09:13:11 -0500

    Implemented jQuery templates
    Fuck ton of work

--- js/market.js
+++ js/market.js
@@ -1,4 +1,32 @@
 $(function () {
-    //activate our market
-    $('#marketModal').modal('show');
+    var apps = [
+        {
+            title : 'test app',
+            icon : 'http://files.softicons.com/download/application-icons/48px-icons-1-4-by-leonard-schwarz/png/48x48/Map.png',
+            installed : true
+        },
+        {
+            title : 'testing application',
+            icon : 'http://cdn5.iconfinder.com/data/icons/48px_icons_collection_by_neweravin/48/adress_book.png',
+            installed : false
+        },
+        {
+            title : 'test app',
+            icon : 'http://files.softicons.com/download/application-icons/48px-icons-1-4-by-leonard-schwarz/png/48x48/Map.png',
+            installed : true
+        },
+        {
+            title : 'testing application',
+            icon : 'http://cdn5.iconfinder.com/data/icons/48px_icons_collection_by_neweravin/48/flash.png',
+            installed : true
+        }
+    ];
+
+    //yeah...
+    $(document).ready(function() {
+        //activate our market
+        $('#marketModal').modal('show');
+        //test out our template
+        $('#appResultTemplate').tmpl(apps).appendTo('#appResultsBody');
+    });
 })
